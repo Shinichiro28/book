a067c149501fe003af6699d9b5bdbcec6afdc219
chappy <s.erk.ikt@gmail.com>
2022-12-20 21:56:42 +0900

    Create Validation

--- src/main/java/com/chappy/portfolio1/book/controllers/PostController.java
+++ src/main/java/com/chappy/portfolio1/book/controllers/PostController.java
@@ -2,6 +2,8 @@ package com.chappy.portfolio1.book.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,7 +19,12 @@ public class PostController {
     }
 
     @PostMapping("/post/create")
-    public String create(@ModelAttribute Post post){
+    public String create(@Validated @ModelAttribute Post post, BindingResult result){
+        
+        //バリデーションエラー
+        if (result.hasErrors()) {
+            return "post/index";
+        }
         return "post/create";
     }
 }
--- src/main/java/com/chappy/portfolio1/book/models/Post.java
+++ src/main/java/com/chappy/portfolio1/book/models/Post.java
@@ -1,11 +1,19 @@
 package com.chappy.portfolio1.book.models;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class Post {
+    @NotBlank
+    @Size(max = 50)
     private String name;
+
+    @NotBlank
+    @Size(max = 254)
     private String introduction;
 }
