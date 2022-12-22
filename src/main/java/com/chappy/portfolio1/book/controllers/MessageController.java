219455c5d86f2f4b0a3ca283a28f9f5e2ab53a5d
chappy <s.erk.ikt@gmail.com>
2022-12-22 11:57:16 +0900

    Create delete function

--- src/main/java/com/chappy/portfolio1/book/controllers/MessageController.java
+++ src/main/java/com/chappy/portfolio1/book/controllers/MessageController.java
@@ -6,6 +6,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -22,12 +23,14 @@ public class MessageController {
         this.repository = repository;
     }
 
+    //一覧ページ
     @GetMapping("/message")
     public String index(@ModelAttribute Message message, Model model){
         model.addAttribute("messages", repository.findAll());
         return "message/index";
     }
     
+    //投稿
     @PostMapping("/message/create")
     public String create(@Validated @ModelAttribute Message message, BindingResult result, Model model){
 
@@ -36,6 +39,13 @@ public class MessageController {
             return "message/index";
         }
         repository.saveAndFlush(message);
+        return "redirect:/chat/1/message";
+    }
+
+    //削除
+    @GetMapping("/message/delete/{id}")
+    public String remove(@PathVariable long id){
+        repository.deleteById(id);
         return "redirect:/chat/1/message";
     }
 }
